6b69bdd50e4d6fb2118f83fac181f330293e550b
Robert Ball <rob@coder73.com>
2018-05-05 17:40:45 +0100

    refactor tests

--- src/com/coder73/tests/CheckoutTests.java
+++ src/com/coder73/tests/CheckoutTests.java
@@ -11,66 +11,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CheckoutTests {
-    List<PricingRule> _rules = new ArrayList<>();
-    private PricingProvider _pricingProvider;
+    private Checkout _checkout;
 
     @Before
     public void Given() {
-        _pricingProvider = new PricingProvider();
-        _rules.add(new PricingRule('A', 50, 3, 125));
-        _rules.add(new PricingRule('B', 30));
-        _rules.add(new PricingRule('C', 15));
+        PricingProvider pricingProvider = new PricingProvider();
+        List<PricingRule> pricingRules = new ArrayList<>();
+        pricingRules.add(new PricingRule('A', 50, 3, 125));
+        pricingRules.add(new PricingRule('B', 30));
+        pricingRules.add(new PricingRule('C', 15));
+
+        _checkout = new Checkout(pricingProvider, pricingRules);
     }
     @Test
     public void WhenScanningATotalIs50() {
-        Checkout checkout = new Checkout(_pricingProvider, _rules);
-        checkout.scan('A');
-        int result = checkout.total();
+        _checkout.scan('A');
+        int result = _checkout.total();
         Assert.assertEquals(50, result);
     }
 
     @Test
     public void WhenScanningAAATotalIs125() {
-        Checkout checkout = new Checkout(_pricingProvider, _rules);
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('A');
-        int result = checkout.total();
+        _checkout.scan('A');
+        _checkout.scan('A');
+        _checkout.scan('A');
+        int result = _checkout.total();
         Assert.assertEquals(125, result);
     }
 
     @Test
     public void WhenScanningBTotalIs30() {
-        Checkout checkout = new Checkout(_pricingProvider, _rules);
-        checkout.scan('B');
-        int result = checkout.total();
+        _checkout.scan('B');
+        int result = _checkout.total();
         Assert.assertEquals(30, result);
     }
 
     @Test
     public void WhenScanningABTotalIs80() {
-        Checkout checkout = new Checkout(_pricingProvider, _rules);
-        checkout.scan('A');
-        checkout.scan('B');
-        int result = checkout.total();
+        _checkout.scan('A');
+        _checkout.scan('B');
+        int result = _checkout.total();
         Assert.assertEquals(80, result);
     }
 
     @Test
     public void WhenScanningCTotalIs15() {
-        Checkout checkout = new Checkout(_pricingProvider, _rules);
-        checkout.scan('C');
-        int result = checkout.total();
+        _checkout.scan('C');
+        int result = _checkout.total();
         Assert.assertEquals(15, result);
     }
 
     @Test
     public void WhenScanningABCTotalIs95() {
-        Checkout checkout = new Checkout(_pricingProvider, _rules);
-        checkout.scan('A');
-        checkout.scan('B');
-        checkout.scan('C');
-        int result = checkout.total();
+        _checkout.scan('A');
+        _checkout.scan('B');
+        _checkout.scan('C');
+        int result = _checkout.total();
         Assert.assertEquals(95, result);
     }
 }
