ee1db26a987b947b976c5a04d96a4571e46d6679
Robert Ball <rob@coder73.com>
2019-05-04 16:47:06 +0100

    migration

--- src/com/coder73/service/DbPricingService.java
+++ src/com/coder73/service/DbPricingService.java
@@ -14,8 +14,7 @@ public class DbPricingService implements PricingService {
     public List<PricingRule> getPricingRules() {
         List<PricingRule> rules = new ArrayList<>();
         try {
-
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/checkout?user=checkoutuser&password=Pa55w0rd1!&autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Checkout?user=checkoutuser&password=Pa55w0rd1!&autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
             Statement st =  conn.createStatement();
             ResultSet rs = st.executeQuery("Select * from pricing_rules");
 
